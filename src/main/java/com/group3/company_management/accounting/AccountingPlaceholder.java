d437e8963d63559e48b27952ef4a2317ff6b17bf
Minh Thanh <minthanh237@gmail.com>
2026-05-19 10:48:07 +0700

    chore: auto-generate standard modular packages for team

---
+++ src/main/java/com/group3/company_management/accounting/AccountingPlaceholder.java
@@ -0,0 +1,4 @@
+package com.group3.company_management.accounting;
+
+public class AccountingPlaceholder {
+}
---
+++ src/main/java/com/group3/company_management/inventory/InventoryPlaceholder.java
@@ -0,0 +1,4 @@
+package com.group3.company_management.inventory;
+
+public class InventoryPlaceholder {
+}
